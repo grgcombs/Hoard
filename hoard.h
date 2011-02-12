9ad2bb71c97054cd29cd01713f700d8682e89b7d
Jonathan Sterling <jonsterling@me.com>
2011-02-12 01:19:27 -0800

    Documented the rest of the header file

--- hoard.h
+++ hoard.h
@@ -23,12 +23,34 @@ struct hoard {
   // collection until you ask them to be; using array subscripts like this will
   // cause your `hoard` to be treated as an array.
   id operator[](NSUInteger i) const;
-
+  
+  // You can also take a slice of a hoard by passing an `NSRange` into
+  // `operator[]`. This will return a new `hoard`. This means that something
+  // like the following is possible:
+  // 
+  //     NSRange rn = NSMakeRange(0,2);
+  //     id arr = col[rn]; // => [@"dogs",@"are"]
+  //     id obj = col[rn][0]; // => @"dogs"
+  // 
+  //
   hoard operator[](NSRange r) const;
+
+  // Finally, you can also find all the indices at which an object occurs by
+  // passing it into `operator[]`.
   NSIndexSet *operator[](id o) const;
   
+  // The default cast operator will yield an object that is guaranteed to
+  // conform to `<NSFastEnumeration>`; this is useful for `for`-loops. For more
+  // specific conversions, use `get<T>`.
   operator id <NSFastEnumeration> () const;
   
+  // Using `get<T>`, you can convert the hoard into one of the Cocoa
+  // collections (and `std::vector` as well):
+  //
+  //     id arr = col.get<NSArray*>(); // the same as the underlying form
+  //     id set = col.get<NSSet*>(); // order is now irrelevant
+  //     id dict = col.get<NSDictionary*>(); // groups elements by twos
+  //     std::vector<id> vec = col.get<std::vector<id>>();
   template <class T> T get() const;
       
 private:
